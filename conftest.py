2bcd554af71c476a6c6f3d3f409a832e87337618
IvanProProgger <i.shelukhin@internet.ru>
2023-07-18 10:53:06 +0300

    fix1

--- conftest.py
+++ conftest.py
@@ -2,10 +2,11 @@ import pytest
 from appium import webdriver
 
 
+desired_caps = {}
+desired_caps["app"] = "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App"
+
 @pytest.fixture(scope="function")
 def driver():
-    desired_caps = {}
-    desired_caps["app"] = "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App"
     driver = webdriver.Remote(
         command_executor='http://127.0.0.1:4723',
         desired_capabilities=desired_caps
